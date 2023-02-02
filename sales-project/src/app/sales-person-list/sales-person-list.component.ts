279b2095b012d999314db502ba932ad0d732fe06
youssefabidi13 <youssef.abidi@etu.uae.ac.ma>
2023-02-02 10:02:55 +0100

    commit

--- sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -3,7 +3,7 @@ import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
-  templateUrl: './sales-person-list.component.html',
+  templateUrl: 'sales-person-list-bootstrap.component.html',
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent {
